fab7426589e11c10f4e6f7f805e3e1ae10cd5bb2
HANAN <124711628+Hanan-Adem@users.noreply.github.com>
2025-05-16 16:53:41 +0300

    Add project files.

---
+++ RentalApp/Form1.Designer.cs
@@ -0,0 +1,125 @@
+﻿namespace RentalApp
+{
+    partial class LoginForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Logintitel = new Label();
+            btnlogin = new Button();
+            Usertxt = new Label();
+            Passtxt = new Label();
+            usertxtBox = new TextBox();
+            passtxtBox = new TextBox();
+            SuspendLayout();
+            // 
+            // Logintitel
+            // 
+            Logintitel.AutoSize = true;
+            Logintitel.Font = new Font("Segoe UI", 15F);
+            Logintitel.ForeColor = Color.CornflowerBlue;
+            Logintitel.ImageAlign = ContentAlignment.TopCenter;
+            Logintitel.Location = new Point(241, 69);
+            Logintitel.Name = "Logintitel";
+            Logintitel.Size = new Size(165, 41);
+            Logintitel.TabIndex = 0;
+            Logintitel.Text = "Login Page";
+            Logintitel.Click += label1_Click;
+            // 
+            // btnlogin
+            // 
+            btnlogin.ForeColor = SystemColors.Highlight;
+            btnlogin.Location = new Point(243, 310);
+            btnlogin.Name = "btnlogin";
+            btnlogin.Size = new Size(163, 34);
+            btnlogin.TabIndex = 1;
+            btnlogin.Text = "Login";
+            btnlogin.UseVisualStyleBackColor = true;
+            // 
+            // Usertxt
+            // 
+            Usertxt.AutoSize = true;
+            Usertxt.ForeColor = SystemColors.Highlight;
+            Usertxt.Location = new Point(106, 166);
+            Usertxt.Name = "Usertxt";
+            Usertxt.Size = new Size(91, 25);
+            Usertxt.TabIndex = 2;
+            Usertxt.Text = "Username";
+            // 
+            // Passtxt
+            // 
+            Passtxt.AutoSize = true;
+            Passtxt.ForeColor = SystemColors.Highlight;
+            Passtxt.Location = new Point(106, 230);
+            Passtxt.Name = "Passtxt";
+            Passtxt.Size = new Size(87, 25);
+            Passtxt.TabIndex = 3;
+            Passtxt.Text = "Password";
+            Passtxt.Click += label3_Click;
+            // 
+            // usertxtBox
+            // 
+            usertxtBox.Location = new Point(227, 165);
+            usertxtBox.Name = "usertxtBox";
+            usertxtBox.Size = new Size(236, 31);
+            usertxtBox.TabIndex = 4;
+            usertxtBox.TextChanged += textBox1_TextChanged;
+            // 
+            // passtxtBox
+            // 
+            passtxtBox.Location = new Point(227, 229);
+            passtxtBox.Name = "passtxtBox";
+            passtxtBox.Size = new Size(236, 31);
+            passtxtBox.TabIndex = 4;
+            passtxtBox.TextChanged += textBox1_TextChanged;
+            // 
+            // LoginForm
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(640, 450);
+            Controls.Add(passtxtBox);
+            Controls.Add(usertxtBox);
+            Controls.Add(Passtxt);
+            Controls.Add(Usertxt);
+            Controls.Add(btnlogin);
+            Controls.Add(Logintitel);
+            Name = "LoginForm";
+            Text = "LoginForm";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label Logintitel;
+        private Button btnlogin;
+        private Label Usertxt;
+        private Label Passtxt;
+        private TextBox usertxtBox;
+        private TextBox passtxtBox;
+    }
+}
---
+++ RentalApp/Form2.Designer.cs
@@ -0,0 +1,129 @@
+﻿namespace RentalApp
+{
+    partial class Form2
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            IbTitel = new Label();
+            btnManageUsers = new Button();
+            btnViewProperties = new Button();
+            btnInvoices = new Button();
+            btnReports = new Button();
+            btnLogout = new Button();
+            SuspendLayout();
+            // 
+            // IbTitel
+            // 
+            IbTitel.AutoSize = true;
+            IbTitel.Font = new Font("Segoe UI", 15F);
+            IbTitel.ForeColor = SystemColors.Highlight;
+            IbTitel.Location = new Point(187, 35);
+            IbTitel.Name = "IbTitel";
+            IbTitel.Size = new Size(258, 41);
+            IbTitel.TabIndex = 0;
+            IbTitel.Text = "Admin Dashboard";
+            IbTitel.Click += IbTitel_Click;
+            // 
+            // btnManageUsers
+            // 
+            btnManageUsers.ForeColor = SystemColors.Highlight;
+            btnManageUsers.Location = new Point(221, 120);
+            btnManageUsers.Name = "btnManageUsers";
+            btnManageUsers.Size = new Size(191, 56);
+            btnManageUsers.TabIndex = 1;
+            btnManageUsers.Text = "Manage Users";
+            btnManageUsers.UseVisualStyleBackColor = true;
+            // 
+            // btnViewProperties
+            // 
+            btnViewProperties.ForeColor = SystemColors.Highlight;
+            btnViewProperties.Location = new Point(221, 182);
+            btnViewProperties.Name = "btnViewProperties";
+            btnViewProperties.Size = new Size(191, 55);
+            btnViewProperties.TabIndex = 2;
+            btnViewProperties.Text = "View All Properties";
+            btnViewProperties.UseVisualStyleBackColor = true;
+            // 
+            // btnInvoices
+            // 
+            btnInvoices.ForeColor = SystemColors.Highlight;
+            btnInvoices.Location = new Point(221, 243);
+            btnInvoices.Name = "btnInvoices";
+            btnInvoices.Size = new Size(191, 53);
+            btnInvoices.TabIndex = 3;
+            btnInvoices.Text = "Manage Invoices";
+            btnInvoices.UseVisualStyleBackColor = true;
+            // 
+            // btnReports
+            // 
+            btnReports.ForeColor = SystemColors.Highlight;
+            btnReports.Location = new Point(221, 302);
+            btnReports.Name = "btnReports";
+            btnReports.Size = new Size(191, 53);
+            btnReports.TabIndex = 4;
+            btnReports.Text = "Reports";
+            btnReports.UseVisualStyleBackColor = true;
+            // 
+            // btnLogout
+            // 
+            btnLogout.Font = new Font("Segoe UI", 10F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            btnLogout.ForeColor = SystemColors.Highlight;
+            btnLogout.Location = new Point(54, 395);
+            btnLogout.Name = "btnLogout";
+            btnLogout.Size = new Size(112, 45);
+            btnLogout.TabIndex = 5;
+            btnLogout.Text = "Logout";
+            btnLogout.UseVisualStyleBackColor = true;
+            // 
+            // Form2
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(610, 478);
+            Controls.Add(btnLogout);
+            Controls.Add(btnReports);
+            Controls.Add(btnInvoices);
+            Controls.Add(btnViewProperties);
+            Controls.Add(btnManageUsers);
+            Controls.Add(IbTitel);
+            Name = "Form2";
+            Text = "AdminForm";
+            Load += Form2_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label IbTitel;
+        private Button btnManageUsers;
+        private Button btnViewProperties;
+        private Button btnInvoices;
+        private Button btnReports;
+        private Button btnLogout;
+    }
+}
